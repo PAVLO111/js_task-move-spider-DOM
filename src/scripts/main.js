7d8f78ff80783756f29e90ed4f8a8f6b88eb6c4b
Pavlo <nadazasada@gmail.com>
2026-05-20 22:52:09 +0300

    spider run with JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  let x = e.clientX;
+  let y = e.clientY;
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
+  // spider.styles.backgroundColor = 'red';
+  spider.styles.display = 'none';
+
+
 });
+
